0a1fa5c56cb51f18ec6c4fc8e3f8b3477a59f3ac
MedeirosMarcelo <marcelomedeiros-@hotmail.com>
2014-09-04 16:37:17 -0300

    Alterado Rigidbody para CharacterController

---
+++ Assets/Scripts/PlatformNoCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformNoCollision : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+	}
+	
+	void OnTriggerStay(Collider col){
+
+		if (col.collider.tag == "Player"){
+			print ("OnTriggerStay");
+			if (col.collider.transform.position.y > this.transform.position.y){
+				Physics.IgnoreCollision(GameObject.Find("Arthur").collider, transform.parent.collider, false);
+			}
+			else {
+				Physics.IgnoreCollision(GameObject.Find("Arthur").collider, transform.parent.collider, true);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycast : MonoBehaviour {
+	
+	public float groundedRaycastLength = 2f;
+	public float walljumpRaycastLength = 0.9f;
+	private RaycastHit walljumpRaycastHit;
+	public Vector3 raycastPoint{ get; private set;}
+	public bool grounded;
+	private int layerMask = 1 << 8;
+	
+	void Update () {
+		IsGrounded ();
+		CanWallJump ();
+		grounded = IsGrounded();
+	}
+	
+	public bool IsGrounded(){
+		
+		RaycastHit raycastHit;
+		var down = -(transform.TransformDirection(Vector3.up));
+		Debug.DrawRay(transform.position, down * groundedRaycastLength, Color.yellow);
+		
+		if (Physics.Raycast(transform.position, down, out raycastHit, groundedRaycastLength, layerMask)){
+			if(raycastHit.collider.tag == "Map"){
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	
+	void OnPlatform(){
+		
+		var down = -(transform.TransformDirection(Vector3.up));
+		Debug.DrawRay(transform.position, down * groundedRaycastLength, Color.yellow);
+		
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, down, groundedRaycastLength, layerMask);
+		if (hit){
+			if (hit.collider.name == "Platform_OneWay"){
+				//	transform.parent.GetComponent<MovementController>().OnPlatform = true;
+			}
+			else {
+				//	transform.parent.GetComponent<MovementController>().OnPlatform = false;
+			}
+		}
+		else {
+			//	transform.parent.GetComponent<MovementController>().OnPlatform = false;
+		}
+	}
+	
+	public bool CanWallJump(){
+		
+		var right = transform.TransformDirection(Vector3.right);
+		Debug.DrawRay(transform.position, right * walljumpRaycastLength, Color.yellow);
+		
+		if (Physics.Raycast(transform.position, right, out walljumpRaycastHit, walljumpRaycastLength)){
+			print (walljumpRaycastHit.collider.name);
+			if(walljumpRaycastHit.collider.tag == "Map"){ 
+				raycastPoint = walljumpRaycastHit.point;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+	
