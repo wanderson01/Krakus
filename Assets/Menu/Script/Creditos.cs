9d97c13778eda7f436c7b4a6e5fb222bcf437093
wanderson01 <anderson_alar@hotmail.com>
2014-11-14 17:23:20 -0200

    integração do menu
    adicionado menu.

---
+++ Assets/Menu/Script/Creditos.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creditos : MonoBehaviour {
+	public string[] _Creditos;
+	public GameObject _Parent;
+	public GameObject Texto;
+	void Start () {
+		for(int i=0; i<_Creditos.Length; i++)
+		{
+
+			Texto.GetComponent<TextMesh>().text = _Creditos[i];
+			Instantiate(Texto,new Vector2(this.transform.position.x,this.transform.position.y+(i*5)),Quaternion.identity);
+		}
+	}
+
+	void Update () {
+
+	}
+}
